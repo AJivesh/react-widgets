de52e2255e9ee4fadd2ccd8bec14ffac4f96ffc9
JIVESH ARYA <jivesh.arya@gmail.com>
2022-02-10 20:06:38 +0530

    ready to deploy

---
+++ src/components/Translate.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+import Convert from "./Convert";
+
+const options = [
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Aarabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="">
+          <label> Enter text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a Lanaguage"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
+};
+
+export default Translate;
